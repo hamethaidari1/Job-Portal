36716e6b7f1165f3c962260e5f6ab7bee97cea26
HAMET HAIDARI <147204499+hamethaidari1@users.noreply.github.com>
2025-12-28 00:37:50 +0300

    Initial commit

---
+++ .history/config/database_20251228001503.js
@@ -0,0 +1,27 @@
+// config/database.js
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// Sequelize bağlantısı oluşturuluyor
+const sequelize = new Sequelize(
+    process.env.DB_NAME || 'job_portal_updatedl',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASSWORD || 'Hamedrasa1212',
+    {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306,
+        logging: false, // Konsol kirliliğini önlemek için
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        dialectOptions: {
+           
+        }
+    }
+);
+
+module.exports = sequelize;
