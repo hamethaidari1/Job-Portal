bc894097e3f6560022a733ceddeea7d2f191b8d5
HAMET HAIDARI <147204499+hamethaidari1@users.noreply.github.com>
2025-12-31 19:14:51 +0300

    use mysql url

--- config/database.js
+++ config/database.js
@@ -1,22 +1,8 @@
-// config/database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,   // railway database name
-  process.env.MYSQLUSER,       // railway mysql user
-  process.env.MYSQLPASSWORD,   // railway mysql password
-  {
-    host: process.env.MYSQLHOST, // railway mysql host
-    port: process.env.MYSQLPORT, // railway mysql port
-    dialect: 'mysql',
-    logging: false,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }
-);
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: 'mysql',
+  logging: false,
+});
 
 module.exports = sequelize;
